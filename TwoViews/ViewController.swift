bc9d39eb2b2bbf0cccaedfb07f8698bdedd6ce8e
Alexey Kanaev <kingkonu@me.com>
2023-03-14 23:52:06 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Alexey Kanaev on 3/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
